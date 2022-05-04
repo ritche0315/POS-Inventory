912ed976eb347652271e997edcae0d4a44fbb707
ritche0315 <ritche2000@gmail.com>
2022-05-04 20:37:05 +0800

    updated file

--- views/admin-view.php
+++ views/admin-view.php
@@ -82,8 +82,32 @@ if(empty($_SESSION['loginSession']) || $_SESSION['loginSession'] == "Invalid"){
 
             
             <div class="main-content">
-                <div class="demo-content">
-
+                <div class="row-1">
+                    <div class="title-wrapper">
+                        <p>Dashboard</p>
+                    </div>
+                    <div class="date-container">
+                        <div class="wrapper">
+                            <i class="fa-solid fa-calendar"></i>
+                            <span id="date"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-2">
+                    <div class="col-4-items">
+                        <div class="item-1"></div>
+                        <div class="item-2"></div>
+                        <div class="item-3"></div>
+                        <div class="item-4"></div>
+                    </div>
+                    <br>
+                </div>
+                
+                <div class="row-3">
+                    <div class="wrapper">
+                        <div class="pie-chart-wrapper"></div>
+                        <div class="bar-graph-wrapper"></div>
+                    </div>
                 </div>
             </div>
         </div>
